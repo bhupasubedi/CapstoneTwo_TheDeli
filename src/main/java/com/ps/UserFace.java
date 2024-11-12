d167a2021923bf11ba7670014ef83f5aa950416e
Bhupa Subedi <bhupa_subedi@yahoo.com>
2024-11-11 19:48:07 -0600

    Added all the classes

---
+++ src/main/java/com/ps/UserFace.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserFace {
+    private ArrayList<SandWich> sandWiches = new ArrayList<>();
+    private Scanner IntScanner = new Scanner(System.in);
+    private Scanner StringScanner = new Scanner(System.in);
+
+
+}
